028f0561178c22c04c7429db9c7b7d9c208e81c1
HonzasikCZ <94143975+HonzasikCZ@users.noreply.github.com>
2025-03-02 13:58:29 +0100

    Update ChannelListener.java

--- src/main/java/io/github/spigotrce/paradiseclientfabric/listener/ChannelListener.java
+++ src/main/java/io/github/spigotrce/paradiseclientfabric/listener/ChannelListener.java
@@ -20,12 +20,12 @@ public class ChannelListener implements Listener {
         try {
             if (Objects.equals(channelName, "minecraft:register") || Objects.equals(channelName, "REGISTER")) // 1.13 channel or 1.8 channel
                 for (String splitted : buf.toString(Charset.defaultCharset()).split("\000")) {
-                    Helper.printChatMessage("&fChannel: &" + (ParadiseClient_Fabric.NETWORK_MOD.getRegisteredChannelsByName().contains(splitted) ? "c " : "d ") + splitted);
+                    Helper.printChatMessage("&fChannel&7: &" + (ParadiseClient_Fabric.NETWORK_MOD.getRegisteredChannelsByName().contains(splitted) ? "1 " : "b ") + splitted);
                     if (ParadiseClient_Fabric.NETWORK_MOD.getRegisteredChannelsByName().contains(splitted))
                         Helper.showNotification("Exploit found!", splitted);
                 }
             else
-                Helper.printChatMessage("&fChannel: &d" + channelName + " &fData: &d" + buf.toString(Charset.defaultCharset()));
+                Helper.printChatMessage("&fChannel&7: &b" + channelName + " &fData&7: &b" + buf.toString(Charset.defaultCharset()));
         } catch (Exception e) {
             Helper.printChatMessage("&4Error handling listener for payload for channel: " + channelName + " " + e.getMessage());
             Constants.LOGGER.error("&4Error handling listener for channel: {} {}", channelName, e);
